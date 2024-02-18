8ec24106b36e0d71984a7b30b6a2fc8bd788bc57
Dejan Ribič <dejan.ribic@gmail.com>
2024-02-18 18:38:33 +0100

    Added lualine plugin. Deleted couple of empty lines.
    Signed-off-by: Dejan Ribič <dejan.ribic@gmail.com>

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -7,6 +7,6 @@ return {
       "MunifTanjim/nui.nvim",
    },
    config = function()
-      vim.keymap.set('n', '<C-n>', ':Neotree filesystem reveal left<CR>', { noremap = true, silent = true })
+      vim.keymap.set('n', '<C-n>', ':Neotree filesystem reveal left<CR>', {})
    end
 }
--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -4,4 +4,3 @@ vim.cmd("set softtabstop=3")
 vim.cmd("set shiftwidth=3")
 vim.g.mapleader = " "
 
-
