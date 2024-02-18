5050c6f7e6f118624c9f1b04c3b8b14a1499d903
Dejan Ribič <dejan.ribic@gmail.com>
2024-02-18 16:35:47 +0100

    Breaking up the config.
    Signed-off-by: Dejan Ribič <dejan.ribic@gmail.com>

---
+++ lua/plugins.lua
@@ -0,0 +1,18 @@
+return{
+   { "catppuccin/nvim", name = "catppuccin", priority = 999 },
+   {
+      'nvim-telescope/telescope.nvim', tag = '-1.1.5',
+      dependencies = { 'nvim-lua/plenary.nvim' }
+   },
+   {"nvim-treesitter/nvim-treesitter", build = ":TSUpdate"},
+   {
+      "nvim-neo-tree/neo-tree.nvim",
+      branch = "v2.x",
+      dependencies = {
+         "nvim-lua/plenary.nvim",
+         "nvim-tree/nvim-web-devicons",
+         "MunifTanjim/nui.nvim",
+      }
+   }
+}
+
