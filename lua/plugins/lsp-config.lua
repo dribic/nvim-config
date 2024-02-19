dff614685d0b3391660c351af5c7b764636aff65
Dejan Ribič <dejan.ribic@gmail.com>
2024-02-19 15:02:22 +0100

    Finished LSP settings. Added telescope-select-ui plugin. Added .luarc.json file to gitignore.
    Signed-off-by: Dejan Ribič <dejan.ribic@gmail.com>

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -10,7 +10,19 @@ return {
       config = function()
          require("mason-lspconfig").setup({
             ensure_installed = { "lua_ls", "rust_analyzer", "gopls", "rnix", "marksman", "jedi_language_server" },
-         }) 
+         })
+      end
+   },
+   {
+      "neovim/nvim-lspconfig",
+      config = function()
+         local lspconfig = require('lspconfig')
+         lspconfig.lua_ls.setup({})
+         lspconfig.rust_analyzer.setup({})
+         lspconfig.gopls.setup({})
+         vim.keymap.set({ 'n', 'v' }, '<leader>ca', vim.lsp.buf.code_action, {})
+         vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
+         vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
       end
    }
 }
