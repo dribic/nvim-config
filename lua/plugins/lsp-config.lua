604bb0117b50a789e2b0d003ed5df25d158599c4
Dejan Ribič <dejan.ribic@gmail.com>
2024-02-25 23:53:26 +0100

    Added Lua formatter, and tested it out on Lua files, and it works.
    Signed-off-by: Dejan Ribič <dejan.ribic@gmail.com>

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,28 +1,28 @@
 return {
-   {
-      "williamboman/mason.nvim",
-      config = function()
-         require("mason").setup()
-      end
-   },
-   {
-      "williamboman/mason-lspconfig.nvim",
-      config = function()
-         require("mason-lspconfig").setup({
-            ensure_installed = { "lua_ls", "rust_analyzer", "gopls", "rnix", "marksman", "jedi_language_server" },
-         })
-      end
-   },
-   {
-      "neovim/nvim-lspconfig",
-      config = function()
-         local lspconfig = require('lspconfig')
-         lspconfig.lua_ls.setup({})
-         lspconfig.rust_analyzer.setup({})
-         lspconfig.gopls.setup({})
-         vim.keymap.set({ 'n', 'v' }, '<leader>ca', vim.lsp.buf.code_action, {})
-         vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
-         vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
-      end
-   }
+	{
+		"williamboman/mason.nvim",
+		config = function()
+			require("mason").setup()
+		end,
+	},
+	{
+		"williamboman/mason-lspconfig.nvim",
+		config = function()
+			require("mason-lspconfig").setup({
+				ensure_installed = { "lua_ls", "rust_analyzer", "gopls", "rnix", "marksman", "jedi_language_server" },
+			})
+		end,
+	},
+	{
+		"neovim/nvim-lspconfig",
+		config = function()
+			local lspconfig = require("lspconfig")
+			lspconfig.lua_ls.setup({})
+			lspconfig.rust_analyzer.setup({})
+			lspconfig.gopls.setup({})
+			vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
+			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+		end,
+	},
 }
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,13 +1,27 @@
 return {
-   "nvim-treesitter/nvim-treesitter",
-   build = ":TSUpdate",
-   config = function()
-      local config = require("nvim-treesitter.configs")
-      config.setup ({
-         ensure_installed = { "c", "cpp", "lua", "vim", "vimdoc", "query", "rust", "go", "python", "xml", "yaml",
-                             "toml", "bash", "gitignore" },
-         highlight = { enable = true },
-         indent = { enable = true },
-      })
-   end
+	"nvim-treesitter/nvim-treesitter",
+	build = ":TSUpdate",
+	config = function()
+		local config = require("nvim-treesitter.configs")
+		config.setup({
+			ensure_installed = {
+				"c",
+				"cpp",
+				"lua",
+				"vim",
+				"vimdoc",
+				"query",
+				"rust",
+				"go",
+				"python",
+				"xml",
+				"yaml",
+				"toml",
+				"bash",
+				"gitignore",
+			},
+			highlight = { enable = true },
+			indent = { enable = true },
+		})
+	end,
 }
